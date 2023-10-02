65cc65bca71362782de9bc47cde569dfa6069260
Lilly <korakot.jarunongkran@gmail.com>
2023-10-02 17:33:59 +0700

    feat: add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { useAuth } from "./contexts/authentication";
 import AuthenticatedApp from "./pages/AuthenticatedApp.jsx";
 import axios from "axios";
 import UnauthenticatedApp from "./pages/UnauthenticatedApp";
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = "https://pet-sitter.up.railway.app";
 
 function App() {
   const auth = useAuth();
